9abea4d99af56e63e2a4048e1aeb1b1603554ed8
casualmatt <matteo@rezzin.ch>
2024-02-17 11:37:29 +0100

    chore: Nuxt configuration and package dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,44 +1,53 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 
-  extends: ['@nuxt/ui-pro'],
+  site: {
+    // production URL
+    url: 'https://docs.postalserver.io/',
+  },
+
+
+  extends: ["@nuxt/ui-pro"],
   modules: [
-    '@nuxt/content',
-    '@nuxt/ui',
-    '@nuxthq/studio',
-    '@nuxtjs/fontaine',
-    '@nuxtjs/google-fonts',
-    'nuxt-og-image'
+    "@nuxt/content",
+    "@nuxt/ui",
+    "@nuxthq/studio",
+    "@nuxtjs/fontaine",
+    "@nuxtjs/google-fonts",
+    "nuxt-og-image",
+    "@nuxthq/studio",
   ],
   hooks: {
     // Define `@nuxt/ui` components as global to use them in `.md` (feel free to add those you need)
-    'components:extend': (components) => {
-      const globals = components.filter((c) => ['UButton', 'UIcon'].includes(c.pascalName))
+    "components:extend": (components) => {
+      const globals = components.filter((c) =>
+        ["UButton", "UIcon"].includes(c.pascalName)
+      );
 
-      globals.forEach((c) => c.global = true)
-    }
+      globals.forEach((c) => (c.global = true));
+    },
   },
   ui: {
-    icons: ['heroicons', 'simple-icons']
+    icons: ["heroicons", "simple-icons"],
   },
   // Fonts
   fontMetrics: {
-    fonts: ['DM Sans']
+    fonts: ["DM Sans"],
   },
   googleFonts: {
-    display: 'swap',
+    display: "swap",
     download: true,
     families: {
-      'DM+Sans': [400, 500, 600, 700]
-    }
+      "DM+Sans": [400, 500, 600, 700],
+    },
   },
   routeRules: {
-    '/api/search.json': { prerender: true },
+    "/api/search.json": { prerender: true },
   },
   devtools: {
-    enabled: true
+    enabled: true,
   },
   typescript: {
-    strict: false
-  }
-})
+    strict: false,
+  },
+});
