3353b80c6cdbaf4df29952f5fa0274438f323646
casualmatt <matteo@rezzin.ch>
2024-03-01 12:05:00 +0100

    Refactor index.vue component

--- pages/index.vue
+++ pages/index.vue
@@ -1,32 +1,48 @@
 <script setup lang="ts">
-const { data: page } = await useAsyncData('index', () => queryContent('/').findOne())
+const { data: page } = await useAsyncData("index", () =>
+  queryContent("/").findOne()
+);
 
 useSeoMeta({
   title: page.value.title,
   ogTitle: page.value.title,
   description: page.value.description,
-  ogDescription: page.value.description
-})
+  ogDescription: page.value.description,
+});
 
 defineOgImage({
-  component: 'Docs',
+  component: "Docs",
   title: page.value.title,
-  description: page.value.description
-})
+  description: page.value.description,
+});
 </script>
 
 <template>
   <div>
     <ULandingHero v-if="page.hero" v-bind="page.hero">
       <template #headline>
-        <UBadge v-if="page.hero.headline" variant="subtle" size="lg" class="relative rounded-full font-semibold">
-          <NuxtLink :to="page.hero.headline.to" target="_blank" class="focus:outline-none" tabindex="-1">
+        <UBadge
+          v-if="page.hero.headline"
+          variant="subtle"
+          size="lg"
+          class="relative rounded-full font-semibold"
+        >
+          <NuxtLink
+            :to="page.hero.headline.to"
+            target="_blank"
+            class="focus:outline-none"
+            tabindex="-1"
+          >
             <span class="absolute inset-0" aria-hidden="true" />
           </NuxtLink>
 
           {{ page.hero.headline.label }}
 
-          <UIcon v-if="page.hero.headline.icon" :name="page.hero.headline.icon" class="ml-1 w-4 h-4 pointer-events-none" />
+          <UIcon
+            v-if="page.hero.headline.icon"
+            :name="page.hero.headline.icon"
+            class="ml-1 w-4 h-4 pointer-events-none"
+          />
         </UBadge>
       </template>
 
@@ -34,12 +50,19 @@ defineOgImage({
         <MDC :value="page.hero.title" />
       </template>
 
-      <MDC :value="page.hero.code" tag="pre" class="prose prose-primary dark:prose-invert mx-auto" />
+      <UColorModeImage
+        light="https://docs.postalserver.io/screenshot.png"
+        dark="https://docs.postalserver.io/screenshot.png"
+      />
     </ULandingHero>
 
     <ULandingSection :title="page.features.title" :links="page.features.links">
       <UPageGrid>
-        <ULandingCard v-for="(item, index) of page.features.items" :key="index" v-bind="item" />
+        <ULandingCard
+          v-for="(item, index) of page.features.items"
+          :key="index"
+          v-bind="item"
+        />
       </UPageGrid>
     </ULandingSection>
   </div>
